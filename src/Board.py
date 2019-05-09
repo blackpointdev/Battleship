6ead5081eb44c2e6982dcde42ffafaa58748b5f3
Marcin Wiśnios <marcinw1998@gmail.com>
2019-05-09 21:16:11 +0200

    Fixes for for Board class, added BoardSegment

--- src/Board.py
+++ src/Board.py
@@ -1,21 +1,43 @@
 import pygame as py
 
+class BoardSegment:
+    def __init__(self, x, y, surface):
+        # self.__x = x
+        # self.__y = y
+        # self.__width = 40
+        # self.__height = 40
+        self.__rect = py.Rect(x, y, 40, 40)
+        self.__surf = surface
+
+    def make_active(self):
+        py.draw.rect(self.__surf, (255, 0, 0), (self.__x + 10, self.__y + 10, 20, 20), 1)
+
+    def draw(self):
+        py.draw.rect(self.__surf, (255, 255, 255),self.__rect, 1)
+
+
 class Board:
     """Representation of board/sea"""
     def __init__(self, x, y, surface, title):
         self.__x = x
         self.__y = y
         self.__surf = surface
+        self.__segments = []
 
-    def draw(self):
+        # Generating board
         x = self.__x
         y = self.__y
         for i in range(10):
             for j in range(10):
-                py.draw.rect(self.__surf, (255, 255, 255), (x, y, 40, 40), 1)
+                seg = BoardSegment(x, y, self.__surf)
+                self.__segments.append(seg)
                 x += 39
             y += 39
             x = self.__x
+
+    def draw(self):
+        for i in self.__segments:
+            i.draw()
 
         font = py.font.SysFont("timesnewroman", 23)
         text_top = [font.render(x, True, (225, 225, 225)) for x in ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']]
--- src/Game.py
+++ src/Game.py
@@ -3,6 +3,9 @@ import sys
 from src import Board
 
 py.init()
+py.display.set_caption("Battleship - Marcin Wiśnios")
+ico = py.image.load("../res/icon.png")
+py.display.set_icon(ico)
 
 size = width, height = 1000, 540
 black = 0, 0, 0
@@ -12,6 +15,8 @@ screen = py.display.set_mode(size)
 board_player = Board.Board(40, 80, screen, "Player")
 board_ai = Board.Board(560, 80, screen, "AI")
 
+tmp_mouse = (0, 0)
+
 while 1:
     for event in py.event.get():
         if event.type == py.QUIT: sys.exit()
@@ -20,3 +25,8 @@ while 1:
     board_player.draw()
     board_ai.draw()
     py.display.flip()
+
+    mouse_pos = py.mouse.get_pos()
+    if mouse_pos != tmp_mouse:
+        print(py.mouse.get_pos())
+        tmp_mouse = mouse_pos
